ab592c04eb90d6ed14146d4b7319df6c19e3df59
XSmeagolX <XSmeagolX@gmx.net>
2018-07-31 15:09:37 +0200

    small code changes

--- web/nav.php
+++ web/nav.php
@@ -32,64 +32,64 @@
 			<div class = "collapse navbar-collapse justify-content-end" id = "navbarNav">
 				<ul class = "navbar-nav">
 <?php if ( $hasBoinctasks ): ?>
-						<li class = 'nav-item'>
-							<a class = 'nav-link btn btn-neutral btn-simple' href = '<?=$linkBoinctasks ?>'><i class="fas fa-list"></i> <?=$linkNameBoinctasks ?></a>
-						</li>
+					<li class = 'nav-item'>
+						<a class = 'nav-link btn btn-neutral btn-simple' href = '<?=$linkBoinctasks ?>'><i class="fas fa-list"></i> <?=$linkNameBoinctasks ?></a>
+					</li>
 <?php endif; ?>
 <?php if ( $hasPendings ): ?>
-						<li class = 'nav-item'>
+					<li class = 'nav-item'>
 						<a class = 'nav-link btn btn-neutral btn-simple' href = '<?=$linkPendings ?>'><i class="fas fa-sync"></i> <?=$linkNamePendings ?></a>
-						</li>
+					</li>
 <?php endif ; ?>
 <?php if ( $hasTeamHp ): ?>
-						<li class = 'nav-item'>
+					<li class = 'nav-item'>
 						<a class = 'nav-link btn btn-neutral btn-simple' href = '<?=$teamHpURL ?>' target = '_new'><i class="fas fa-globe"></i> <?=$teamHpName ?></a>
-						</li>
+					</li>
 <?php endif; ?>
 <?php if ( $hasBoincstats ): ?>
-						<li class = 'nav-item'>
+					<li class = 'nav-item'>
 						<a class = 'nav-link btn btn-neutral btn-simple' href = '<?=$linkBoincstats ?>' target = '_new'><i class="fas fa-chart-bar"></i> <?=$linkNameBoincstats ?></a>
-						</li>
+					</li>
 <?php endif; ?>
 <?php if ( $hasSGStats ): ?>
-						<li class = 'nav-item'>
+					<li class = 'nav-item'>
 						<a class = 'nav-link btn btn-neutral btn-simple' href = '<?=$linkSGStats ?>' target = '_new'><i class="fas fa-chart-bar"></i> <?=$linkNameSGStats ?></a>
-						</li>
+					</li>
 <?php endif; ?>
 <?php if ( $hasWcg ): ?>
-						<li class = 'nav-item'>
+					<li class = 'nav-item'>
 						<a class = 'nav-link btn btn-neutral btn-simple' href = '<?=$linkWcg ?>' target = '_new'><i class="fas fa-globe"></i> <?=$linkNameWcg ?></a>
-						</li>
+					</li>
 <?php endif; ?>
 <?php if ($showLinks): ?>
-						<li class = "nav-item">
-							<a class = "nav-link btn btn-neutral" href = "<?=$hp_nav_link01 ?>"><?=$hp_nav_name01 ?></a>
-						</li>
-						<li class = "nav-item">
-							<a class = "nav-link btn btn-neutral" href = "<?=$hp_nav_link02 ?>"><?=$hp_nav_name02 ?></a>
-						</li>
-						<li class = "nav-item">
-							<a class = "nav-link btn btn-neutral" href = "<?=$hp_nav_link03 ?>"><?=$hp_nav_name03 ?></a>
-						</li>
+					<li class = "nav-item">
+						<a class = "nav-link btn btn-neutral btn-simple" href = "<?=$hp_nav_link01 ?>"><?=$hp_nav_name01 ?></a>
+					</li>
+					<li class = "nav-item">
+						<a class = "nav-link btn btn-neutral btn-simple" href = "<?=$hp_nav_link02 ?>"><?=$hp_nav_name02 ?></a>
+					</li>
+					<li class = "nav-item">
+						<a class = "nav-link btn btn-neutral btn-simple" href = "<?=$hp_nav_link03 ?>"><?=$hp_nav_name03 ?></a>
+					</li>
 <?php endif; ?>
 <?php if ($showMoreLinks): ?>
-						<li class = "nav-item dropdown">
-							<a class = "nav-link btn dropdown-toggle" href = "#" id = "dropdownId" data-toggle = "dropdown" aria-haspopup = "true" aria-expanded = "true"><i class="fas fa-info-circle"></i> <?=$showMoreLinksName ?></a>
-							<div class = "dropdown-menu dropdown-menu-right" aria-labelledby = "dropdownId">
-								<a class = "dropdown-item" href = "https://github.com/XSmeagolX" target = "_new"><i class="fab fa-github"></i> Userstats on GitHub</a>
-								<a class = "dropdown-item" href = "https://boinc-userstats.de" target = "_new"><i class="fas fa-home"></i> Userstats Website</a>
-								<div class = "dropdown-divider"></div>
-								<a class = "dropdown-item" href = "https://www.seti-germany.de" target = "_new"><i class="fas fa-globe"></i> SETI.Germany</a>
-								<a class = "dropdown-item" href = "https://join.worldcommunitygrid.org/?recruiterId=653215&teamId=4VVG5BDPP1" target = "_new"><i class="fas fa-globe"></i> World Community Grid</a>
-							</div>
-						</li>
+					<li class = "nav-item dropdown">
+						<a class = "nav-link btn dropdown-toggle" href = "#" id = "dropdownId" data-toggle = "dropdown" aria-haspopup = "true" aria-expanded = "true"><i class="fas fa-info-circle"></i> <?=$showMoreLinksName ?></a>
+						<div class = "dropdown-menu dropdown-menu-right" aria-labelledby = "dropdownId">
+							<a class = "dropdown-item" href = "https://github.com/XSmeagolX" target = "_new"><i class="fab fa-github"></i> Userstats on GitHub</a>
+							<a class = "dropdown-item" href = "https://boinc-userstats.de" target = "_new"><i class="fas fa-home"></i> Userstats Website</a>
+							<div class = "dropdown-divider"></div>
+							<a class = "dropdown-item" href = "https://www.seti-germany.de" target = "_new"><i class="fas fa-globe"></i> SETI.Germany</a>
+							<a class = "dropdown-item" href = "https://join.worldcommunitygrid.org/?recruiterId=653215&teamId=4VVG5BDPP1" target = "_new"><i class="fas fa-globe"></i> World Community Grid</a>
+						</div>
+					</li>
 <?php endif; ?>
 <?php if (!$didVersionEdit): ?>
 <?php if ($setUpdatecheck): ?>
 <?php if ( $update_available ): ?>
-								<li class = 'nav-item'>
-								<a class = 'nav-link' href = 'check_update.php' class = 'btn btn-neutral btn-simple textgelb'><i class="textgelb fas fa-exclamation-circle"></i><font class = 'textgelb'> <?=$linkNameUpdate ?></font></a>
-								</li>
+					<li class = 'nav-item'>
+						<a class = 'nav-link btn btn-neutral btn-simple textgelb' href = 'check_update.php' class = ''><i class="textgelb fas fa-exclamation-circle"></i><font class = 'textgelb'> <?=$linkNameUpdate ?></font></a>
+					</li>
 <?php endif; ?>
 <?php endif; ?>
 <?php endif; ?>
