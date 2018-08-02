47af87cdd6d58481be62d6f8c4bad75db6921e85
XSmeagolX <XSmeagolX@gmx.net>
2018-08-02 18:15:40 +0200

    include version control for dev-versions

--- web/version.php
+++ web/version.php
@@ -1,5 +1,10 @@
 <?php
 	# Bitte hier nichts manuell ändern, da ansonsten die Update-Prüfung falsche Ergebnisse liefert!
 	# Please do not change anything, because the update check will get false results!
-	$userstats_version = "5.1.1";	
+	$userstats_version = "5.1.1";
+	# Nur true, wenn kein Release
+	# is true, wenn not release
+	$dev = true;
+	$devNr = "10";
+	$dev_version = "5.1.2-dev".$devNr;
 ?>
