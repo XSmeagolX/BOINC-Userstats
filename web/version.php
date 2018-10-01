f52f62990658c682f11f8c3d2f6202f93f9ed443
XSmeagolX <XSmeagolX@gmx.net>
2018-10-01 15:35:24 +0200

    Release 5.2.1

--- web/version.php
+++ web/version.php
@@ -1,7 +1,7 @@
 <?php
 	# Bitte hier nichts manuell ändern, da ansonsten die Update-Prüfung falsche Ergebnisse liefert!
 	# Please do not change anything, because the update check will get false results!
-	$userstats_release_version = "5.2.0";
+	$userstats_release_version = "5.2.1";
 	# Nur true, wenn kein Release
 	# is true, wenn not release
 	$dev = false;
