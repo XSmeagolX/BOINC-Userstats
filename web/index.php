0c3ab74743ec98279e614d81fa87aed58cf063a0
XSmeagolX <XSmeagolX@gmx.net>
2019-05-30 13:58:52 +0200

    dev10

--- web/index.php
+++ web/index.php
@@ -1,14 +1,14 @@
 <?php
-	include "./settings/settings.php";
-	include "./functions/get_lang.php";
+	include("./settings/settings.php");
+	include("./functions/get_lang.php");
 
 	$showProjectHeader = false;
 	$showTasksHeader = false;
 	$showUpdateHeader = false;
 	$showErrorHeader = false;
 
-	include "./functions/initialize_variables.php";
-	include "./functions/get_userdata.php";
+	include("./functions/initialize_variables.php");
+	include("./functions/get_userdata.php");
 
 	if ($cpid === "") {
 		$showFreeDCBadges = false;
@@ -16,92 +16,11 @@
 		$linkFreeDCBadges = $linkFreeDCBadges.$cpid;
 	}
 
-	include "./functions/get_Timestamps.php";
-
-	include "./functions/get_TotalCredits.php";
-
-	$query_getAllProjects = mysqli_query($db_conn, "SELECT * FROM boinc_grundwerte ORDER BY project ASC");
-	while ($row = mysqli_fetch_assoc($query_getAllProjects)) {
-		if ($row["project_status"] <= 1) {
-			$hasactiveProject = true;			
-			$shortname = $row["project_shortname"];
-			$table_row["project_name"] = $row["project"];
-			$table_row["total_credits"] = $row["total_credits"];
-			$table_row["project_home_link"] = $row["project_homepage_url"];
-			$table_row["user_stats_vorhanden"] = $row["project_status"];
-			if ($hasXML) {
-				$table_row["xml"] = $row["xml"];
-			}
-			$query_getOutput1h = mysqli_query($db_conn,"SELECT sum(credits) AS sum1h FROM boinc_werte WHERE project_shortname = '" . $shortname . "' AND time_stamp>'" . $einsh . "'");
-			$row2 = mysqli_fetch_assoc($query_getOutput1h);
-			$table_row["sum1h"] = $row2["sum1h"];
-			$sum1h_total += $table_row["sum1h"];
-			
-			$query_getOutput2h = mysqli_query($db_conn,"SELECT sum(credits) AS sum2h FROM boinc_werte WHERE project_shortname = '" . $shortname . "' AND time_stamp>'" . $zweih . "'");
-			$row2 = mysqli_fetch_assoc($query_getOutput2h);
-			$table_row["sum2h"] = $row2["sum2h"];
-			$sum2h_total += $table_row["sum2h"];
-			
-			$query_getOutput6h = mysqli_query($db_conn,"SELECT sum(credits) AS sum6h FROM boinc_werte WHERE project_shortname = '" . $shortname . "' AND time_stamp>'" . $sechsh . "'");
-			$row2 = mysqli_fetch_assoc($query_getOutput6h);
-			$table_row["sum6h"] = $row2["sum6h"];
-			$sum6h_total += $table_row["sum6h"];
-			
-			$query_getOutput12h = mysqli_query($db_conn,"SELECT sum(credits) AS sum12h FROM boinc_werte WHERE project_shortname = '" . $shortname . "' AND time_stamp>'" . $zwoelfh . "'");
-			$row2 = mysqli_fetch_assoc($query_getOutput12h);
-			$table_row["sum12h"] = $row2["sum12h"];
-			$sum12h_total += $table_row["sum12h"];
-					
-			$query_getOutputToday = mysqli_query($db_conn,"SELECT sum(credits) AS sum_today FROM boinc_werte WHERE project_shortname = '" . $shortname . "' AND time_stamp > '" . $tagesanfang . "'");
-			$row2 = mysqli_fetch_assoc($query_getOutputToday);
-			$table_row["sum_today"] = $row2["sum_today"];
-			$sum_today_total += $table_row["sum_today"];
-			
-			$query_getOutputYesterday = mysqli_query($db_conn,"SELECT sum(credits) AS sum_yesterday FROM boinc_werte WHERE project_shortname = '" . $shortname . "' AND time_stamp > '" . $gestern_anfang . "' AND time_stamp < '" . $gestern_ende . "'");
-			$row2 = mysqli_fetch_assoc($query_getOutputYesterday);
-			$table_row["sum_yesterday"] = $row2["sum_yesterday"];
-			$sum_yesterday_total += $table_row["sum_yesterday"];
-			
-			$table_row["proz_anteil"] = sprintf("%01.2f", $row["total_credits"] * 100 / $sum_total);
-			$table_row["project_link"] = "project.php?projectid=" . $shortname . "";
-			$table_row["retired"] = false;
-			
-			$table[] = $table_row;
-			$pie_array = $table_row;
-			
-			} else {
-
-			$hasretiredProject = true;			
-			$shortname = $row["project_shortname"];
-			$table_row["project_name"] = $row["project"];
-			$table_row["total_credits"] = $row["total_credits"];
-			$table_row["project_home_link"] = $row["project_homepage_url"];
-			$table_row["user_stats_vorhanden"] = $row["project_status"];
-			$table_row["proz_anteil"] = sprintf("%01.2f", $row["total_credits"] * 100 / $sum_total);
-			$table_row["project_link"] = "project.php?projectid=" . $shortname . "";
-			$table_row["retired"] = true;
-			$total_credits_retired = $total_credits_retired + $row["total_credits"];
-			$table_retired[] = $table_row;
-			$pie_array = $table_row;
-		}
-
-		if ($table_row["proz_anteil"] >= $separat && !$table_row["retired"]) {
-			$pie_html .= "	['" . $pie_array["project_name"] . "',	 " . round($pie_array["total_credits"] * 100 / $sum_total, 2) . "],\n";
-			} else {
-			if (!$table_row["retired"]) $pie_other += ($pie_array["total_credits"] * 100 / $sum_total);
-			else $pie_other_retired += ($pie_array["total_credits"] * 100 / $sum_total);
-		}		
-	}
-	
-	if ($pie_other > 0) {
-		$pie_html .= "	['" . $tr_ch_pie_zwp . "',	 " . round($pie_other, 2) . "],\n";
-	}
-	if ($pie_other_retired > 0) {
-		$pie_html .= "	['" . $tr_ch_pie_ret . "',	 " . round($pie_other_retired, 2) . "]\n";
-	}
-
+	include("./functions/get_Timestamps.php");
+	include("./functions/get_TotalCredits.php");
+	include("./functions/get_AllProjects.php");
 	include("./functions/get_output_html.php");
-	include("./functions/get_output_gesamt_html.php"); 
+	include("./functions/get_output_gesamt_html.php");
 
 	include("./header.php"); 
 
