84445bf9d5de0ed61e76fa2748198e2afb3621e0
XSmeagolX <XSmeagolX@gmx.net>
2019-11-24 21:49:14 +0100

    added a logging feature to cronjob

--- cronjob/cronjob.php
+++ cronjob/cronjob.php
@@ -3,6 +3,18 @@
 	// Set the absolute path to your boinc_db_connect.php file
 	include "../database/boinc_db_connect.php";
 
+	// Aktiviere das Logging
+	// Beachte: Du musst den Aufruf des cronjob entsprechend abändern, damit der Output nicht in die Konsole sondern in eine Datei geschrieben wird
+	// füge folgendes noch an den Aufruf an das Ende der Zeile:  2>&1 | tee /var/log/boinc-userstats.log
+	// es sollte dann in etwa so ausshen: 0 * * * * php /usr/local/bin/cronjob/cronjob.php 2>&1 | tee /var/log/boinc-userstats.log
+	// enable Logging
+	// you have to edit your crontab to write the output of this logging into a file an not to console
+	// paste at the end of the line for your cronjob:  2>&1 | tee /var/log/boinc-userstats.log 
+	// it should look like this: 0 * * * * php /usr/local/bin/cronjob/cronjob.php 2>&1 | tee /var/log/boinc-userstats.log
+	$set_logging = false;
+	//$set_logging = true;
+
+
 	// Setze hier den Zeitpunkt von Mitternacht. 
 	// Dies wird für die Berechnung der Werte für die Gesamt Credits eines Tages in deiner Zeitzone benötigt, welche in den Gesamt-Charts dargestellt wird
 	// Deine Zeitzonenbezeichnung. Wird in der Infobar neben den Zeitangabe für das letzte Update angezeigt. 
@@ -56,6 +68,9 @@
 	$unixtime = time();
 	$timestamp_hour = ceil($unixtime/3600) * 3600;
 	$timestamp_timezone = date(('Y-m-d H').':00:00', $unixtime);
+	if ($set_logging) {
+		echo "Start: ".date(('Y-m-d H:i:s'), $unixtime)."</ br>";
+	}
 
 	$query_getUserData = mysqli_query($db_conn, "SELECT * from boinc_user");
 	while ($row = mysqli_fetch_assoc($query_getUserData)) {
@@ -69,6 +84,11 @@
 			<h1 class = "title">ACHTUNG -- ATTENTION</h1>
 			<h3 class = "description text-center">Daten dürfen nur 1x pro Stunde abgerufen werden!!<br>Data could be fetched only once per hour!</h3>
 		</div>';
+		$updateendtime = time();
+		if ($set_logging) {
+			echo "
+Ende: ".date(('Y-m-d H:i:s'), $updateendtime)."</ br>";
+		}
 		exit;
 	}
 
@@ -78,6 +98,10 @@
 	$query=mysqli_query($db_conn,"SELECT * FROM boinc_grundwerte WHERE project_status = 1;") or die (mysqli_error());	
 	while($row=mysqli_fetch_assoc($query))
 	{
+		if ($set_logging) {
+			echo "
+ Projekt: ".$row[project]."</ br>";
+		}
 		$xml_string = false;
 		$xml_string = @file_get_contents ($row['url'] . "show_user.php?userid=" . $row['project_userid'] . "&format=xml", 0, $ctx);
 		$xml = @simplexml_load_string($xml_string);
@@ -97,6 +121,13 @@
 			if (!mysqli_query($db_conn,$sql)) {
 				echo("Error description: " . mysqli_error($db_conn));
 			} 
+			if ($set_logging) {
+				echo "Projektdaten eingetragen.</ br>";
+			}
+		} else {
+			if ($set_logging) {
+				echo "keine neuen Projektdaten.</ br>";
+			}
 		}
 		
 		$sql = "UPDATE boinc_grundwerte 
@@ -104,11 +135,19 @@
 		if (!mysqli_query($db_conn,$sql)) {
 			echo("Error description: " . mysqli_error($db_conn));
 		}
-		
+
 		if ($isMidnight) {
 			$sql= "INSERT INTO boinc_werte_day (project_shortname, time_stamp, total_credits) 
 			VALUES ('" .$row['project_shortname']. "', '" .$timestamp_hour. "', ".$total_credits.")";
 			mysqli_query($db_conn,$sql);
+			if ($set_logging) {
+				echo "
+ Mitternachtsdaten in Tabelle eingetragen.</ br>";
+			}
+		}
+		if ($set_logging) {
+			echo "
+			-----------------------------------------------------</ br></ br>";
 		}
 	}
 
@@ -117,6 +156,10 @@
 		VALUES ('gesamt', '" .$timestamp_hour. "','" .$total_credits_hour. "')";
 		if (!mysqli_query($db_conn,$sql)) {
 			echo("Error description: " . mysqli_error($db_conn));
+		}
+		if ($set_logging) {
+			echo "
+ Gesamtwerte in Tabelle eingetragen.</ br>";
 		}
 	}
 	
@@ -129,9 +172,19 @@
 		$sql_gesamt= "INSERT INTO boinc_werte_day (project_shortname, time_stamp, total_credits) 
 		VALUES ('gesamt', '" .$timestamp_hour. "', '" .$total_credits_gestern. "')";
 		mysqli_query($db_conn,$sql_gesamt);
+		if ($set_logging) {
+			echo "
+ Mitternachts Gesamtdaten in Tabelle eingetragen.</ br>";
+		}
 	}
 	
 	$updateendtime = time();
 	$sqlupdateendtime = "UPDATE boinc_user SET lastupdate='" .$updateendtime. "'";
 	mysqli_query($db_conn,$sqlupdateendtime);
+
+	if ($set_logging) {
+		echo "
+Ende: ".date(('Y-m-d H:i:s'), $updateendtime)."</ br>";
+	}
 ?>
+
